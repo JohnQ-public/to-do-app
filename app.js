c83af9412e7f672c3a195fc43896177f1459b7c9
John P Quense <jpquense@gmail.com>
2017-11-18 10:16:30 -0500

    assignment-2.8.1 open id issue with  delete button

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-function onReady() { 
-  let id = 0;
+function onReady() {
+
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  
+  let id = 0;
+
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }
-    
+
     toDos.push({
       title: newToDoText.value,
       complete: false,
-      id:
+      id: id
     });
-    
     id++;
-    
     newToDoText.value = '';
     renderTheUI();
   }
-  
+
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-    
+
     toDoList.textContent = '';
-    
+
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
@@ -31,26 +30,26 @@ function onReady() {
       const deleteLi = document.createElement('button');
       deleteLi.innerText = 'Delete';
       newLi.textContent = toDo.title;
-      
+
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
       newLi.appendChild(deleteLi);
-      
+
       deleteLi.addEventListener('click', remove => {
-        const selectLi = toDos.filter(select => toDo.id == idS)
-        toDos.splice(toDo.id);
+        const selectLi = toDos.filter(select => toDo.id === newLi.id)
+        toDos.splice(selectLi, 1);
         renderTheUI();
       })
     });
   }
-  
+
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
-  
+
   renderTheUI();
-  
+
  }
 
 
@@ -58,4 +57,3 @@ window.onload = function() {
   alert('The window has loaded!');
   onReady();
 };
-
