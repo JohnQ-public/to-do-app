602a10142d8e72c162161f10e9a1a3c4526ba5ff
John P Quense <jpquense@gmail.com>
2017-11-18 07:55:25 -0500

    modify id probliem

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-function onReady() { 
-  
+function onReady() {
+
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  
+
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }
-    
+
     toDos.push({
       title: newToDoText.value,
       complete: false
     });
-    
+
     newToDoText.value = '';
-    
+
     renderTheUI();
   }
-  
+
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-    
+
     toDoList.textContent = '';
-    
+
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('input');
       const checkbox = document.createElement('checkbox');
       checkbox.type = 'checkbox';
-      
+
       newLi.textContent = toDo.title;
-      
+
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
     });
   }
-  
+
   addToDoForm.addEventListener('submit', event => {
+    console.log(event)
     event.preventDefault();
     createNewToDo();
   });
-  
+
   renderTheUI();
-  
+
  }
 
 
 window.onload = function() {
-  alert('The window has loaded!');
+  console.log('The window has loaded!');
   onReady();
 };
-
